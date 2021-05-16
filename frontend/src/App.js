8c1bbb5c0834cb4b2a31a541e85c116669ae83c7
Alejandro Cáceres <alejandro.caceres@wardenautomation.com>
2021-05-15 22:19:57 -0400

    Reach-Bootstrap Setup, Header & Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { Container } from "react-bootstrap"
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div>
-      My app 
+      <Header />
+        <main className="py-5">
+          <Container>
+            <h1>Welcome to ProShop</h1>
+          </Container>
+        </main>
+      <Footer />
     </div>
   );
 }
